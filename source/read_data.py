5d63c80b811846c084ddfe0374f9d2721097c26d
Anurag Bhatt <anuraag.bhatt@gmail.com>
2021-10-15 14:48:05 +0200

    reading excel and preliminary modifications.

--- source/read_data.py
+++ source/read_data.py
@@ -1,3 +1,27 @@
-import os
 import pandas as pd
-import sklearn
+from pathlib import Path
+
+PARENT_DIR = Path(__file__).parent.parent
+INPUT_DIR = Path(PARENT_DIR,'input')
+
+INTERESTING_COLUMNS = ['a','b','c','alpha','beta','gamma','sg']
+
+def read_xlsx(fpath):
+    df = pd.read_excel(fpath)
+    return df
+
+def modify_df(df):
+    modified_df = df[INTERESTING_COLUMNS]
+    modified_df = modified_df[modified_df.alpha == 90].reset_index(drop=1)
+    modified_df = modified_df[modified_df.beta == 90].reset_index(drop=1)
+    modified_df = modified_df[modified_df.gamma == 90].reset_index(drop=1)
+
+    print(modified_df.sample(5))
+    print(modified_df.shape)
+    print(len(modified_df.sg.unique()))
+    return modified_df
+
+if __name__ == "__main__":
+    fpath = Path(INPUT_DIR,'Data.xlsx')
+    raw_df = read_xlsx(fpath)
+    subset_df = modify_df(raw_df)
