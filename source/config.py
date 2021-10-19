f60fbb2f43f570e262fea0cf9964d456bc8a7e8b
Anurag Bhatt <anuraag.bhatt@gmail.com>
2021-10-19 22:36:51 +0200

    moving settings to config, data dumping to excel, preliminary decision tree classifier.

---
+++ source/config.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+N_SG = 3
+CUBIC = True
+PARENT_DIR = Path(__file__).parent.parent
+INPUT_DIR = Path(PARENT_DIR,'input')
+OUTPUT_DIR = Path(PARENT_DIR,'output')
+INTERESTING_COLUMNS = ['a','b','c','alpha','beta','gamma','sg']
